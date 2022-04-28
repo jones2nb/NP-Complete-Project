1c895a22cc1358eb7746eceb36814018bd1ac4c5
mcclasjw <mcclasjw@dukes.jmu.edu>
2022-04-27 21:39:16 -0400

    working exact solution

--- exact_solution/exact_solution.py
+++ exact_solution/exact_solution.py
@@ -4,26 +4,56 @@ Exact solution for the max clique problem
 Author: Jacob McClaskey
 Date: April 25, 2022
 """
-def clique(graph, vertex):
-    neighbors = graph[vertex]
-    clique = [vertex]
-    for neighbor in neighbors:
-        temp = graph[neighbor]
-        for vertex in temp:
-            if vertex in neighbors and vertex not in clique:
-                clique.append(vertex)
+from zeroconf import itertools
 
-    return len(clique), clique
+
+def is_clique(graph, test_clique):
+
+    for vertex in test_clique:
+        print(f"Test Clique is: {test_clique}")
+        print(f"edges for {vertex} are: {graph[vertex]}")
+        if test_clique.issubset(graph[vertex]):
+            continue
+        else:
+            return False
+    return True
+
+
+# def clique(graph, vertex):
+#     neighbors = graph[vertex]
+#     clique = graph[vertex]
+#     clique.add(vertex)
+#     for neighbor in neighbors:
+#         # print(f"temp: {temp}")
+#         # print(f"cliqur: {clique}")
+#         temp = clique.intersection(graph[neighbor])
+#         if len(temp) > len(clique):
+#             clique = temp
+#         # print(clique)
+
+#     return len(clique), clique
 
 def max_clique(graph):
     max_clique = 0
-    cliques = []
+    found_clique = []
     for vertex in graph.keys():
-        clique_size, found_clique = clique(graph, vertex)
-        if clique_size > max_clique:
-            max_clique = clique_size
-            cliques = found_clique
-    return max_clique, cliques
+        for i in range(1, len(graph[vertex])):
+            for path in itertools.combinations(graph[vertex], i):
+                clique = set(path)
+                # print(clique)
+                # print(f"i: {i}")
+                # print(f"vertex: {vertex}")
+                if len(clique) > max_clique and is_clique(graph, clique):
+                    if len(clique) > max_clique:
+                        max_clique = len(clique)
+                        found_clique = clique
+                    
+    # for vertex in graph.keys():
+    #     clique_size, found_clique = clique(graph, vertex)
+    #     if clique_size > max_clique:
+    #         max_clique = clique_size
+    #         cliques = found_clique
+    return max_clique, found_clique
 
 
 """
@@ -35,7 +65,7 @@ def main():
     graph = dict()
     for _ in range(num_vert):
         vertex = input().split()
-        graph[vertex[0]] = set(vertex[1:])
+        graph[vertex[0]] = set(vertex)
     # print(graph)
     size, clique = max_clique(graph)
     print(f"largest clique: {size}")
