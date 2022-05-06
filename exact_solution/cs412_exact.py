5235512a697fbc5db46505d36837eb9d68d50772
Mcclaskey-jw <81594548+Mcclaskey-jw@users.noreply.github.com>
2022-05-06 18:07:09 -0400

    reformatting my part of the repo

---
+++ exact_solution/cs412_exact.py
@@ -0,0 +1,69 @@
+"""
+Exact solution for the max clique problem 
+
+Though process take in a graph loop over every vertex and then create a lists of every combination of potential cliques.
+Then check to see if the potential clique is a subset of every node in the potential set. IE that every vertex in the test clique,
+should contain the test clique as a subset for the vertexs edges, else the test clique is not a clique
+
+Author: Jacob McClaskey
+Date: April 25, 2022
+"""
+import itertools
+import time
+
+"""
+Simple function to test if a given clique is in fact a clique.
+Loop through the clique to see if the given clique is a subset of every edge list for each of the vertices in the given clique.
+"""
+def is_clique(graph, test_clique):
+    for vertex in test_clique:
+        # print(f"Test Clique is: {test_clique}")
+        # print(f"edges for {vertex} are: {graph[vertex]}")
+        if not test_clique.issubset(graph[vertex]):
+            return False
+    return True
+
+"""
+Simple function that takes a graph and loops over ever vertice and creates all potential combination of subsets of the edges,
+and then check to see if given subset of edges is an actual clique.
+"""
+def max_clique(graph):
+    max_clique = 0
+    found_clique = []
+    for vertex in graph.keys():
+        for i in range(1, len(graph[vertex])):
+            for path in itertools.combinations(graph[vertex], i):
+                clique = set(path)
+                # print(clique)
+                # print(f"i: {i}")
+                # print(f"vertex: {vertex}")
+                clique.add(vertex)
+                # if is_clique(graph, clique):
+                if len(clique) > max_clique and is_clique(graph, clique):
+                    if len(clique) > max_clique:
+                        max_clique = len(clique)
+                        found_clique = clique
+    return max_clique, found_clique
+
+
+"""
+Input to the function should be an int stating how many vertices there are in the graph.
+Followed by each vertices and the edges they connect too.
+"""
+def main():
+    num_vert = int(input())
+    graph = dict()
+    for _ in range(num_vert):
+        vertex = input().split()
+        graph[vertex[0]] = set(vertex)
+    # print(graph)
+    t0 = time.time()
+    size, clique = max_clique(graph)
+    t1 = time.time() - t0
+    print(f"largest clique: {size}")
+    print(f"vertex in the clique are: {clique}")
+    # print(f"Time elpsed: {t1} seconds")
+
+
+if __name__ == "__main__":
+    main()
--- exact_solution/run_test_cases.sh
+++ exact_solution/run_test_cases.sh
@@ -1,29 +1,29 @@
 # This is the test case driver for the exact code
 echo 
-echo Running simple complete graph of size 3 input is test_in1.txt
-time python3 exact_solution.py < test_in1.txt
+echo Running simple complete graph of size 3 input is test_1.txt
+time python3 cs412_exact.py < test_cases/test_1.txt
 echo ==================================================================
 echo 
-echo Running test complete graph size 10 input is medium_test.txt
-time python3 exact_solution.py < medium_test.txt
+echo Running test complete graph size 10 input is test_2.txt
+time python3 cs412_exact.py < test_cases/test_2.txt
 echo ==================================================================
 echo
-echo Running test complete graph size 20 input is graph_size_20.txt
-time python3 exact_solution.py < graph_size_20.txt
+echo Running test complete graph size 20 input is test_3.txt
+time python3 cs412_exact.py < test_cases/test_3.txt
 echo ==================================================================
 echo
-echo Running test random large graph input is Large Input
-time python3 exact_solution.py < ../Large /Input
+echo Running test random large graph input is test_4.txt
+time python3 cs412_exact.py < test_cases/test_4.txt
 echo ==================================================================
 echo
 echo THE FOLLOWING TEST WILL TAKE A LONG TIME TO RUN
 echo
-echo Running test complete graph size 25 input is graph_size_25.txt
-time python3 exact_solution.py < graph_size_25.txt
+echo Running test complete graph size 25 input is test_5.txt
+time python3 cs412_exact.py < test_cases/test_5.txt
 echo ==================================================================
 echo
-echo Running test complete graph size 30 input is graph_size_30.txt
-time python3 exact_solution.py < graph_size_30.txt
+echo Running test complete graph size 30 input is test_6.txt
+time python3 cs412_exact.py < test_cases/test_6.txt
 echo ==================================================================
 echo
 
